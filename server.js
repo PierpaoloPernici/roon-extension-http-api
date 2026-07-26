25c1169ada8483c45413c46f968cc329be2909ab
Pierpaolo Pernici <pierpaolo@gmail.com>
2026-07-26 20:31:09 +0200

    feat: add Swagger API documentation
    Interactive API documentation available at /api-docs
    Uses swagger-jsdoc + swagger-ui-express.
    
    Inspired by @thingspi
    https://github.com/thingspi/roon-extension-http-api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+var swaggerJsdoc = require('swagger-jsdoc');
+var swaggerUi = require('swagger-ui-express');
 
 var app = express();
 const bodyParser = require('body-parser');
@@ -7,7 +9,26 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT=3001;
+const PORT = 3001;
+
+// Swagger configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Roon HTTP API',
+      version: '1.1.0',
+      description: 'HTTP API wrapper for the Roon audio streaming system',
+    },
+    servers: [
+      { url: 'http://localhost:' + PORT, description: 'Local server' },
+    ],
+  },
+  apis: ['./routes.js'],
+};
+
+const swaggerSpecs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.use(express.static(path.join(__dirname, 'htmls')));
 
@@ -16,7 +37,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 
 require('./routes')(app);
 
